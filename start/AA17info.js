aa090b674ecdfcfffabe2d7a820629384d8ba970
kybb0709 <48310106+kybb0709@users.noreply.github.com>
2020-09-16 17:23:39 +0900

    Delete AA17info.js

--- start/AA17info.js
+++
@@ -1,7 +0,0 @@
-//AA17info.js
-module.exports = function (id, name, phone) {
-  console.log("My Info");
-  console.log("ID : " + id);
-  console.log("Name : " + name);
-  console.log("Phone : " + phone + "\n");
-};
